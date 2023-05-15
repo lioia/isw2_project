34e8b49cf6c82fbe9cc63c009c99a18b6649eddb
Alessandro Lioi <lioialessandro2000@live.com>
2023-05-15 10:49:30 +0200

    Moved App to CsvGenerator. Created apps package for executables

---
+++ src/main/java/it/uniroma2/alessandrolioi/apps/CsvAnalysis.java
@@ -0,0 +1,11 @@
+package it.uniroma2.alessandrolioi.apps;
+
+import java.util.logging.Logger;
+
+public class CsvAnalysis {
+    private static final Logger logger = Logger.getLogger("CsvAnalysis");
+
+    public static void main(String[] args) {
+        logger.info("CsvAnalysis");
+    }
+}
