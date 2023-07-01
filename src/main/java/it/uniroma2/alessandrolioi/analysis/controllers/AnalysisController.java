8c5ff6f95224cd0de98decc629dc72a598494ca7
Alessandro Lioi <lioialessandro2000@live.com>
2023-07-01 11:56:59 +0200

    fix(Analysis): Arff files are incremental (follows previous commit)

--- src/main/java/it/uniroma2/alessandrolioi/analysis/controllers/AnalysisController.java
+++ src/main/java/it/uniroma2/alessandrolioi/analysis/controllers/AnalysisController.java
@@ -1,14 +1,19 @@
 package it.uniroma2.alessandrolioi.analysis.controllers;
 
 import it.uniroma2.alessandrolioi.analysis.exceptions.ArffException;
+import it.uniroma2.alessandrolioi.common.DatasetPaths;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
+
+import java.nio.file.Path;
 
 public class AnalysisController {
     public Instances loadInstance(String project, int testingRelease, String instanceType) throws ArffException {
         try {
-            String filename = "dataset/%s/%s-%d.arff".formatted(project, instanceType, testingRelease);
-            Instances instance = ConverterUtils.DataSource.read(filename);
+            Path path = DatasetPaths.fromProject(project)
+                    .resolve("arff")
+                    .resolve(String.format("%s-%d.arff", instanceType, testingRelease));
+            Instances instance = ConverterUtils.DataSource.read(path.toString());
             if (instance.classIndex() == -1)
                 instance.setClassIndex(instance.numAttributes() - 1);
             return instance;
